41de5d4b5a74612ab38940706962f8ea2dc0c775
agartanah <vyatin2017@mail.ru>
2024-10-24 20:57:35 +0700

    add destroy widgets tasks

---
+++ src/main/java/org/example/javalabfx/DestroyWidgets.java
@@ -0,0 +1,48 @@
+package org.example.javalabfx;
+
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+public class DestroyWidgets {
+    static final TextField textField1 = new TextField("Здравствуйте");
+    static final TextField textField2 = new TextField("Привет");
+    static final TextField textField3 = new TextField("Дарова надоел");
+
+    static final CheckBox checkBoxTextField1 = new CheckBox();
+    static final CheckBox checkBoxTextField2 = new CheckBox();
+    static final CheckBox checkBoxTextField3 = new CheckBox();
+
+    static final Button backButton = new Button("Назад");
+
+    public static void start(Stage stage, Stage prevStage) {
+        checkBoxTextField1.setOnAction(event -> textField1.setVisible(checkBoxTextField1.isSelected()));
+        checkBoxTextField2.setOnAction(event -> textField2.setVisible(checkBoxTextField2.isSelected()));
+        checkBoxTextField3.setOnAction(event -> textField3.setVisible(checkBoxTextField3.isSelected()));
+
+        checkBoxTextField1.setSelected(true);
+        checkBoxTextField2.setSelected(true);
+        checkBoxTextField3.setSelected(true);
+
+        backButton.setOnAction(actionEvent -> {
+            stage.close();
+            prevStage.show();
+        });
+
+        VBox widgetsContainer = new VBox(10, textField1, textField2, textField3);
+        VBox checkBoxContainer = new VBox(20, checkBoxTextField1, checkBoxTextField2, checkBoxTextField3);
+
+        HBox mainContainer = new HBox(20, checkBoxContainer, widgetsContainer);
+        mainContainer.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(mainContainer, 400, 200);
+        stage.setScene(scene);
+        stage.setTitle("Аннигиляторная пушка");
+        stage.show();
+    }
+}
--- src/main/java/org/example/javalabfx/Main.java
+++ src/main/java/org/example/javalabfx/Main.java
@@ -1,31 +1,24 @@
 package org.example.javalabfx;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.shape.Circle;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class Main extends Application {
-    private final Button buttonTask1 = new Button("Перекидыватель строк");
-    private final Button buttonTask2 = new Button("Аннигиляция виджетов");
-    private final Button buttonTask3 = new Button();
-    private final Button buttonTask4 = new Button();
-    private final Button buttonTask5 = new Button();
-
+    final Button buttonTask1 = new Button("Перекидыватель строк");
+    final Button buttonTask2 = new Button("Аннигиляторная пушка");
+    final Button buttonTask3 = new Button();
+    final Button buttonTask4 = new Button();
+    final Button buttonTask5 = new Button();
+    final VBox taskButtonsContainer = new VBox(10);
 
     @Override
     public void start(Stage stage) throws IOException {
-        VBox taskButtonsContainer = new VBox(10);
-
         taskButtonsContainer.setAlignment(Pos.CENTER);
 
         buttonTask1.setOnAction(actionEvent -> {
@@ -34,7 +27,14 @@ public class Main extends Application {
             stage.close();
         });
 
+        buttonTask2.setOnAction(actionEvent -> {
+            DestroyWidgets.start(new Stage(), stage);
+
+            stage.close();
+        });
+
         taskButtonsContainer.getChildren().add(buttonTask1);
+        taskButtonsContainer.getChildren().add(buttonTask2);
 
         Scene scene = new Scene(taskButtonsContainer, 400, 400);
         stage.setScene(scene);
